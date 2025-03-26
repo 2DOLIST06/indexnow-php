9fd71672afdb226043a3083edbee784d546c2b2b
2DOLIST06 <contact@2dolist.fr>
2025-03-26 13:37:33 +0100

    Update indexnow_auto.php

--- public/indexnow_auto.php
+++ public/indexnow_auto.php
@@ -58,7 +58,7 @@ if (empty($urlList)) {
 // --- LISTE D’URLS À ENVOYER EN DUR ---
 $urlList = [
     // Mets ici les URLs que tu veux envoyer manuellement
-    'https://www.2dolist.fr/categorie/helicoptere',
+    'https://www.2dolist.fr/categorie/ulm',
 ];
 
 // --- AFFICHE LES URLS POUR LE LOG ---
