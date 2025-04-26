e544e33438f6db30f01078305d30c616ba9627fb
2DOLIST06 <contact@2dolist.fr>
2025-04-26 17:32:23 +0200

    Update indexnow_auto.php

--- public/indexnow_auto.php
+++ public/indexnow_auto.php
@@ -58,7 +58,7 @@ if (empty($urlList)) {
 // --- LISTE D’URLS À ENVOYER EN DUR ---
 $urlList = [
     // Mets ici les URLs que tu veux envoyer manuellement
-    'https://www.2dolist.fr/favicon.ico',
+    'https://www.2dolist.fr/activities/saut-en-parachute-tandem-pres-de-biscarosse',
 
 ];
 
