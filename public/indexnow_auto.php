c2a6c5c3f67783abdbb7ae73acb3767abf253fcf
2DOLIST06 <contact@2dolist.fr>
2025-08-18 22:59:18 -0400

    Update indexnow_auto.php

--- public/indexnow_auto.php
+++ public/indexnow_auto.php
@@ -58,8 +58,9 @@ if (empty($urlList)) {
 // --- LISTE D’URLS À ENVOYER EN DUR ---
 $urlList = [
     // Mets ici les URLs que tu veux envoyer manuellement
-    'https://www.2dolist.fr/activities/bapteme-en-helicoptere-pres-de-brive-la-gallarde',
-    'https://www.2dolist.fr/activities/vol-en-parapente-motorise-pres-de-biarritz',
+    'https://www.2dolist.fr/activities/vol-en-planeur-a-moteur-a-morlaix-dans-le-finistere',
+    'https://www.2dolist.fr/activities/vol-en-ulm-multiaxe-pres-de-narbonne',
+    'https://www.2dolist.fr/activities/bapteme-en-ulm-3-axes-a-nancy',
 
     ];
 
